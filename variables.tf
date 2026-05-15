80db59f99f8d45b920da92c8400fec5e26d7dd5d
Bakhtiyar Khan <your-email@example.com>
2026-05-14 22:53:56 -0500

    Update VM size in variables and terraform configuration for consistency

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "location" {
 variable "vm_size" {
   description = "Azure VM size"
   type        = string
-  default     = "Standard_B2s"
+  default     = "Standard_B1ms"
 }
 
 variable "resource_group_name" {
