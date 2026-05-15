4cec5ce55101524204c8958b9d87a0efb74fef04
Bakhtiyar Khan <your-email@example.com>
2026-05-14 23:11:51 -0500

    Update Azure resources to use Central US region for consistency

--- main.tf
+++ main.tf
@@ -4,21 +4,21 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "finance-tracker-prod-eastus-vnet"
+  name                = "finance-tracker-prod-centralus-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "finance-tracker-prod-eastus-subnet"
+  name                 = "finance-tracker-prod-centralus-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_security_group" "nsg" {
-  name                = "finance-tracker-prod-eastus-nsg"
+  name                = "finance-tracker-prod-centralus-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -48,7 +48,7 @@ resource "azurerm_network_security_group" "nsg" {
 }
 
 resource "azurerm_public_ip" "public_ip" {
-  name                = "finance-tracker-prod-eastus-publicip"
+  name                = "finance-tracker-prod-centralus-publicip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
@@ -56,12 +56,12 @@ resource "azurerm_public_ip" "public_ip" {
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = "finance-tracker-prod-eastus-nic"
+  name                = "finance-tracker-prod-centralus-nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "finance-tracker-prod-eastus-ipconfig"
+    name                          = "finance-tracker-prod-centralus-ipconfig"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
@@ -74,7 +74,7 @@ resource "azurerm_network_interface_security_group_association" "nic_nsg" {
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "finance-tracker-prod-eastus-vm"
+  name                = "finance-tracker-prod-centralus-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = var.vm_size
@@ -94,7 +94,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
-    name                 = "finance-tracker-prod-eastus-osdisk"
+    name                 = "finance-tracker-prod-centralus-osdisk"
   }
 
   source_image_reference {
